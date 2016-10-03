af21b5ac3678c1dde3d83e9ffb0211ff54ee1296
1408721 <trogdorrunner@gmail.com>
2016-10-03 15:35:49 +0100

    php test 2

--- index.php
+++ index.php
@@ -6,7 +6,17 @@
     <body>
         <p>
             <?php
-                echo "Hello World";
+            echo "Hello World";
+            echo "Hello," . " " . "world" . "!";
+            echo 5*7;
+            $myname = "OnyC";
+            $bignumbers = 84877496;
+            if ($bignumbers > 1){
+                print "I wasn't told the difference between echo and print";
+            }
+            echo "Time for the difference";
+            echo "Name: ".$myname.", Age: ".$bignumbers;
+            print "Name: ".$myname.", Age: ".$bignumbers;
             ?>
         </p>
     </body>
