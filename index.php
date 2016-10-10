5e7a1f91440e796fce5e388ed1bae72a2f0ede84
1408721 <trogdorrunner@gmail.com>
2016-10-10 14:41:16 +0100

    added adminer.php

--- index.php
+++ index.php
@@ -12,9 +12,16 @@
             $myname = "OnyC";
             $bignumbers = 84877496;
             if ($bignumbers > 1){
-                print "<p>I wasn't told the difference between echo and print</p>";
+                print "<p>help</p>";
             }
             echo "<p>help</p>";
+            $myArray = array("A", "B", "C");
+            echo $myArray[0];
+            $myArray[1] = "Bee";
+            echo $myArray[1];
+            unset($myArray[2]);
+            array_push($myArray, "See");
+            print count($myArray);
             ?>
         </p>
     </body>
